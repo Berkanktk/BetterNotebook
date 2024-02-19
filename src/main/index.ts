5128916f44df5b3ca16678a4058e96ca6f6b6052
Berkanktk <berkank@live.dk>
2024-02-20 00:30:22 +0100

    Formattings

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,7 +8,7 @@ import fs from 'fs'
 
 let isWindows = process.platform === 'win32'
 let mainWindow: BrowserWindow;
-let lastOpenedFilePath = ''; 
+let lastOpenedFilePath = '';
 
 function createWindow(): void {
   mainWindow = new BrowserWindow({
@@ -44,88 +44,88 @@ function createWindow(): void {
 
   const menu = Menu.buildFromTemplate([
     {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Save',
-                accelerator: 'Ctrl+S',
-                click: () => {
-                  mainWindow.webContents.send('save-file');
-                }
-            },
-            {
-              label: 'Open',
-              accelerator: 'Ctrl+O',
-              click: () => {
-                mainWindow.webContents.send('open-file');
-              }
-            },
-            {
-                label: isWindows ? 'Exit' : 'Quit',
-                accelerator: isWindows ? 'Alt+F4' : 'CmdOrCtrl+Q',
-                click: () => app.quit()
+      label: 'File',
+      submenu: [
+        {
+          label: 'Save',
+          accelerator: 'Ctrl+S',
+          click: () => {
+            mainWindow.webContents.send('save-file');
+          }
+        },
+        {
+          label: 'Open',
+          accelerator: 'Ctrl+O',
+          click: () => {
+            mainWindow.webContents.send('open-file');
+          }
+        },
+        {
+          label: isWindows ? 'Exit' : 'Quit',
+          accelerator: isWindows ? 'Alt+F4' : 'CmdOrCtrl+Q',
+          click: () => app.quit()
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'delete' },
+        { role: 'selectAll' }
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        { role: 'close' }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            const { response } = await dialog.showMessageBox({
+              type: 'info',
+              buttons: ['OK', 'Learn More'],
+              title: 'Learn More',
+              message: 'See features and more on the GitHub page.'
+            })
+            if (response === 1) {
+              await shell.openExternal('https://github.com/Berkanktk/BetterNotebook')
             }
-        ]
-    },
-    {
-        label: 'Edit',
-        submenu: [
-            { role: 'undo' },
-            { role: 'redo' },
-            { type: 'separator' },
-            { role: 'cut' },
-            { role: 'copy' },
-            { role: 'paste' },
-            { role: 'delete' },
-            { role: 'selectAll' }
-        ]
-    },
-    {
-        label: 'Window',
-        submenu: [
-            { role: 'minimize' },
-            { role: 'zoom' },
-            { role: 'close' }
-        ]
-    },
-    {
-        label: 'Help',
-        submenu: [
-            {
-                label: 'Learn More',
-                click: async () => {
-                    const { response } = await dialog.showMessageBox({
-                        type: 'info',
-                        buttons: ['OK', 'Learn More'],
-                        title: 'Learn More',
-                        message: 'See features and more on the GitHub page.'
-                    })
-                    if (response === 1) {
-                        await shell.openExternal('https://github.com/Berkanktk/BetterNotebook')
-                    }
-                }
-            },
-            {
-                label: 'About',
-                click: async () => {
-                    const { response } = await dialog.showMessageBox({
-                        type: 'info',
-                        buttons: ['OK'],
-                        title: 'About',
-                        message: 'An improved notebook experience made using Electron, Svelte and TypeScript.\n\nApp Version: ' + app.getVersion() + '\nElectron Version: ' + process.versions.electron + '\nNode.js Version: ' + process.versions.node + '\nChrome Version: ' + process.versions.chrome
-                    })
-                }
-            },
-            {
-                label: 'Report Issue',
-                click: async () => {
-                    await shell.openExternal('https://github.com/Berkanktk/BetterNotebook/issues')
-                }
-            }
-        ]
+          }
+        },
+        {
+          label: 'About',
+          click: async () => {
+            const { response } = await dialog.showMessageBox({
+              type: 'info',
+              buttons: ['OK'],
+              title: 'About',
+              message: 'An improved notebook experience made using Electron, Svelte and TypeScript.\n\nApp Version: ' + app.getVersion() + '\nElectron Version: ' + process.versions.electron + '\nNode.js Version: ' + process.versions.node + '\nChrome Version: ' + process.versions.chrome
+            })
+          }
+        },
+        {
+          label: 'Report Issue',
+          click: async () => {
+            await shell.openExternal('https://github.com/Berkanktk/BetterNotebook/issues')
+          }
+        }
+      ]
     }
   ])
-  
+
   Menu.setApplicationMenu(menu)
 
 }
@@ -140,7 +140,7 @@ ipcMain.on('open-dialog', (_event) => {
     if (!result.canceled) {
       const content = fs.readFileSync(result.filePaths[0], 'utf8');
       mainWindow.webContents.send('file-opened', content);
-      lastOpenedFilePath = result.filePaths[0]; 
+      lastOpenedFilePath = result.filePaths[0];
       const filename = basename(lastOpenedFilePath);
       mainWindow.setTitle(filename + " - BetterNotebook");
     }
@@ -163,7 +163,7 @@ ipcMain.on('save-dialog', (_event, content) => {
       if (!result.canceled && result.filePath) {
         fs.writeFileSync(result.filePath, content);
         lastOpenedFilePath = result.filePath;
-        const filename = basename(lastOpenedFilePath); 
+        const filename = basename(lastOpenedFilePath);
         mainWindow.setTitle(filename + " - BetterNotebook");
       }
     }).catch(err => {
